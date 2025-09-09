80ab3f9c3703d9d6dd7649f7dad7524161cefab4
aprendizdev <waltergutierrez1100@gmail.com>
2025-09-09 13:21:53 -0500

    validar correo

--- src/app/main/services/forms/applicant-form.service.ts
+++ src/app/main/services/forms/applicant-form.service.ts
@@ -1,21 +1,25 @@
-import { Injectable, signal, WritableSignal } from "@angular/core"
-import { NonNullableFormBuilder, Validators } from "@angular/forms"
-import { ApplicantFormGroup } from "../../models/forms/ApplicantForm"
-import { emailDomainValidator } from "../../validators/email-domain.validator"
+import { Injectable, signal, WritableSignal } from "@angular/core";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { ApplicantFormGroup } from "../../models/forms/ApplicantForm";
+import { emailDomainValidator } from "../../validators/email-domain.validator";
 
 @Injectable({
   providedIn: "root"
 })
 export class ApplicantFormService {
-  applicantForm: WritableSignal<ApplicantFormGroup>
+  applicantForm: WritableSignal<ApplicantFormGroup>;
 
   constructor(private formBuilder: NonNullableFormBuilder) {
     const initialValue = this.formBuilder.group({
       name: this.formBuilder.control("", [Validators.required]),
-      email: this.formBuilder.control("", [Validators.email, Validators.required, emailDomainValidator("@equitel.com.co")]),
+      email: this.formBuilder.control("", [
+        Validators.email,
+        Validators.required,
+        emailDomainValidator(["@equitel.com.co"]) // <-- Aquí está el cambio
+      ]),
       observations: this.formBuilder.control("")
-    })
+    });
 
-    this.applicantForm = signal(initialValue)
+    this.applicantForm = signal(initialValue);
   }
 }
